9014809458f473aee274b0ee23c26955b54579f2
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2020-04-23 00:27:32 -0700

    rust terraform

---
+++ main.tf
@@ -0,0 +1,94 @@
+provider "google" {
+  project         = "securethebox-server"
+  region          = "us-central1"
+  zone            = "us-central1-a"
+}
+
+provider "google-beta" {
+  # credentials = using env value GOOGLE_CLOUD_KEYFILE_JSON
+  project         = "securethebox-server"
+  region          = "us-central1"
+  zone            = "us-central1-a"
+
+  # --release-channel stable
+  release_channel { 
+    channel = "STABLE" 
+  }
+
+  # --addons CloudRun
+  addons_config {
+    istio_config {
+      disabled = false
+    }
+    cloudrun_config {
+      disabled = false
+    }
+  }
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  # --no-enable-basic-auth
+  master_auth {
+    username = ""
+    password = ""
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+
+  # --enable-ip-alias
+  ip_allocation_policy {
+  }
+  
+  default_max_pods_per_node = 8
+
+  network = "projects/securethebox-server/global/networks/default"
+  subnetwork = "projects/securethebox-server/regions/us-central1/subnetworks/default"
+
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+    http_load_balancing {
+      disabled = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = "my-gke-cluster"
+  node_count = 4
+
+  
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+    disk_size_gb = 50
+    image_type = "COS"
+    disk_type = "pd-standard"
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+  }
+}
